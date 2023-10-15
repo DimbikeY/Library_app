2e0109aa33a013687e72be8984a98fd9992ba355
Dmitrii Dolmatov <mr.dolmatov-2001@mail.ru>
2023-10-15 23:18:06 +0300

    Fixed @Repository lack

--- src/main/java/dolmatov/repositories/BooksRepository.java
+++ src/main/java/dolmatov/repositories/BooksRepository.java
@@ -2,9 +2,11 @@ package dolmatov.repositories;
 
 import dolmatov.models.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BooksRepository extends JpaRepository<Book, Integer> {
     public Book findById(int id);
     public List<Book> findByTitleStartingWithIgnoreCase(String title);
--- src/main/java/dolmatov/repositories/PeopleRepository.java
+++ src/main/java/dolmatov/repositories/PeopleRepository.java
@@ -2,8 +2,11 @@ package dolmatov.repositories;
 
 import dolmatov.models.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface PeopleRepository extends JpaRepository<Person, Integer> {
     public Optional<Person> findByName(String name);
     public Person findById(int id);
